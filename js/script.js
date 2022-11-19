01d001dce99cc499bf77cd522b47984cde2d531d
3mr-5aled <moroamr2005@gmail.com>
2022-11-19 23:24:07 +0200

    history and read more function

--- js/script.js
+++ js/script.js
@@ -44,3 +44,22 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+// read more
+
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("readbutton");
+
+    if (dots.style.display === "none") {
+        dots.style.display = "inline";
+        btnText.innerHTML = "Read more";
+        moreText.style.display = "none";
+    } else {
+        dots.style.display = "none";
+        btnText.innerHTML = "Read less";
+        moreText.style.display = "inline";
+    }
+}
